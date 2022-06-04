b48409fe1cd037ae1a0d05cad5c87bf106e0fd48
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-06-04 22:00:59 +0300

    fix sintaxis

--- server/models/index.ts
+++ server/models/index.ts
@@ -1,50 +1,51 @@
 import sequelize from '../db';
 import { DataTypes } from 'sequelize';
 
-const User = sequelize.define('user', {
+export const User = sequelize.define('user', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     email: { type: DataTypes.STRING, unique: true },
     password: { type: DataTypes.STRING },
     role: { type: DataTypes.STRING, defaultValue: 'USER' },
 });
 
-const Basket = sequelize.define('basket', {
+export const Basket = sequelize.define('basket', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
 });
 
-const BasketDevise = sequelize.define('basket_devise', {
+export const BasketDevice = sequelize.define('basket_Device', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
 });
 
-const Devise = sequelize.define('devise', {
+export const Device = sequelize.define('Device', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: { type: DataTypes.STRING, unique: true, allowNull: false },
+    price: { type: DataTypes.INTEGER, allowNull: false },
     rating: { type: DataTypes.INTEGER, defaultValue: 0 },
     img: { type: DataTypes.STRING, allowNull: false },
 });
 
-const Type = sequelize.define('type', {
+export const Type = sequelize.define('type', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: { type: DataTypes.STRING, unique: true, allowNull: false },
 });
 
-const Brand = sequelize.define('brand', {
+export const Brand = sequelize.define('brand', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: { type: DataTypes.STRING, unique: true, allowNull: false },
 });
 
-const Rating = sequelize.define('rating', {
+export const Rating = sequelize.define('rating', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     rate: { type: DataTypes.INTEGER, allowNull: false },
 });
 
-const DeviseInfo = sequelize.define('devise_info', {
+export const DeviceInfo = sequelize.define('Device_info', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     title: { type: DataTypes.STRING, allowNull: false },
     description: { type: DataTypes.STRING, allowNull: false },
 });
 
-const TypeBrand = sequelize.define('type_brand', {
+export const TypeBrand = sequelize.define('type_brand', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
 });
 
@@ -54,25 +55,23 @@ Basket.belongsTo(User);
 User.hasMany(Rating);
 Rating.belongsTo(User);
 
-Basket.hasMany(BasketDevise);
-BasketDevise.belongsTo(Basket);
+Basket.hasMany(BasketDevice);
+BasketDevice.belongsTo(Basket);
 
-Type.hasMany(Devise);
-Devise.belongsTo(Type);
+Type.hasMany(Device);
+Device.belongsTo(Type);
 
-Brand.hasMany(Devise);
-Devise.belongsTo(Brand);
+Brand.hasMany(Device);
+Device.belongsTo(Brand);
 
-Devise.hasMany(Rating);
-Rating.belongsTo(Devise);
+Device.hasMany(Rating);
+Rating.belongsTo(Device);
 
-Devise.hasMany(BasketDevise);
-BasketDevise.belongsTo(Devise);
+Device.hasMany(BasketDevice);
+BasketDevice.belongsTo(Device);
 
-Devise.hasMany(DeviseInfo);
-DeviseInfo.belongsTo(Devise);
+Device.hasMany(DeviceInfo, { as: 'info' });
+DeviceInfo.belongsTo(Device);
 
 Type.belongsToMany(Brand, { through: TypeBrand });
 Brand.belongsToMany(Type, { through: TypeBrand });
-
-export default { User, Basket, BasketDevise, Devise, Type, Brand, Rating, DeviseInfo, TypeBrand };
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import deviseRouter from './deviseRouter';
+import deviceRouter from './deviceRouter';
 import userRouter from './userRouter';
 import brandRouter from './brandRouter';
 import typeRouter from './typeRouter';
 const router = Router();
 
-router.use('/devise', deviseRouter);
+router.use('/device', deviceRouter);
 router.use('/user', userRouter);
 router.use('/brand', brandRouter);
 router.use('/type', typeRouter);
