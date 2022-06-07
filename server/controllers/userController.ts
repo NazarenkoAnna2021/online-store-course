6a180ed988a54b57f3fb90659d133380e31ff432
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-06-07 21:36:49 +0300

    fix codestyle

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -7,10 +7,6 @@ import jwt from 'jsonwebtoken';
 import { Model } from 'sequelize/types';
 import { IUser } from './entities/IUser';
 
-const generateJwt = (id: number, email: string, role: string) => {
-    return jwt.sign({ id, email, role }, String(process.env.SECRET_KEY), { expiresIn: '24h' });
-}
-
 class UserController implements IUserController {
     registration = async (req: Request, res: Response, next: NextFunction) => {
         const { email, password, role } = req.body;
@@ -29,10 +25,9 @@ class UserController implements IUserController {
         const user: Model<IUser> = await User.create({ email, role, password: hashPassword });
         const id = user.getDataValue('id');
         const basket = await Basket.create({ userId: id });
-        const token = generateJwt(id, email, user.getDataValue('password'));
+        const token = this.generateJwt(id, email, role);
 
         return res.json({ token });
-
     }
 
     login = async (req: Request, res: Response, next: NextFunction) => {
@@ -49,17 +44,19 @@ class UserController implements IUserController {
             return next(ApiError.internal('Invalid password'));
         };
 
-        const token = generateJwt(user.getDataValue('id'), email, user.getDataValue('password'));
+        const token = this.generateJwt(user.getDataValue('id'), email, user.getDataValue('password'));
 
         return res.json({ token });
     }
 
     check = async (req: Request, res: Response, next: NextFunction) => {
         const { email, role, id } = req.body;
-        const token = generateJwt(id, email, role);
+        const token = this.generateJwt(id, email, role);
 
         return res.json({ token });
     }
+    
+    private generateJwt = (id: number, email: string, role: string) => jwt.sign({ id, email, role }, String(process.env.SECRET_KEY), { expiresIn: '24h' })
 };
 
 export const userController = new UserController();
